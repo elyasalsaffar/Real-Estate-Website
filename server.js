f0475f2935db2dd44483f99c7dc91e450e699578
elyasalsaffar <elyas.dusty@gmail.com>
2025-05-14 20:53:44 +0300

    Initialized project structure

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const logger = require('morgan')
+const methodOverride = require('method-override')
+const session = require('express-session')
+require('dotenv').config()
+
+const PORT = process.env.PORT ? process.env.PORT : 3000
+
+const db = require('./db')
+
+const app = express()
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+// app.use(
+//   session({
+//     secret: process.env.SESSION_SECRET,
+//     resave: false,
+//     saveUninitialized: true
+//   })
+// )
+
+app.get('/', (req, res) => {
+  res.send('Our app is connected. . . ')
+})
+
+app.listen(PORT, () => {
+  console.log(`Running Server on Port ${PORT} . . . `)
+})
