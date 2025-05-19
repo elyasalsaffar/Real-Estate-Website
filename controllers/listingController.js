f2f07de22a974c8edab2b3f2b5c23158dd0ebc40
elyasalsaffar <elyas.dusty@gmail.com>
2025-05-19 20:41:56 +0300

    Monday's work

--- controllers/listingController.js
+++ controllers/listingController.js
@@ -10,35 +10,46 @@ const getAllListings = async (req, res) => {
     }
 };
 
-// const getSingleListing = async (req, res) => {
-//     try {
-//         const listing = await Listing.findById(req.params.id);
-//         res.render('./listings/show.ejs', { listing });
-//     } catch (error) {
-//         console.error('An error occurred getting a single listing!', error.message);
-//     }
-// };
+const getAllRequests = async (req, res) => {
+    try {
+        const listings = await Listing.find({ isApproved: {$in: [ null, undefined ]} });
+        res.render('./listings/requests.ejs', { listings });
+    } catch (error) {
+        console.error('An error occurred getting all request listings!', error.message);
+    }
+};
 
 const getSingleListing = async (req, res) => {
     try {
-        const { id } = req.params;
-        console.log('Attempting to fetch listing with ID:', id); // ADD THIS LINE
-
-        const listing = await Listing.findById(id);
-
-        if (!listing) {
-            console.log(`Listing with ID ${id} not found.`); // ADD THIS LINE
-            return res.status(404).send('Listing not found');
-        }
-
-        console.log('Successfully fetched listing:', listing); // ADD THIS LINE
+        const listing = await Listing.findById(req.params.id);
         res.render('./listings/show.ejs', { listing });
     } catch (error) {
         console.error('An error occurred getting a single listing!', error.message);
     }
 };
 
-module.exports ={
+const updateIsApprovedById = async (req, res) => { //to be continued
+    try {
+        const listing = await listing.findByIdAndUpdate(req.params.id, req.body, {
+            isApproved: true
+        })
+    } catch (error) {
+        console.error('An error has occurred updating a requests Approve/ Reject status');
+    }
+};
+
+const getAllRequestListings = async (req, res) => {
+    try {
+        const listings = await Listing.find({ isApproved: true });
+        res.render('./listings/requestsList.ejs', { listings });
+    } catch (error) {
+        console.error('An error has occurred getting requests lists');
+    }
+};
+
+module.exports = {
     getAllListings,
-    getSingleListing
+    getSingleListing,
+    getAllRequests,
+    getAllRequestListings
 }
--- routes/listingRouter.js
+++ routes/listingRouter.js
@@ -5,6 +5,10 @@ const listingController = require('../controllers/listingController.js');
 
 router.get('/all', listingController.getAllListings);
 
+router.get('/requests', listingController.getAllRequests);
+
+router.get('/requestsList/:id', listingController.getAllRequestListings);
+
 router.get('/:id', listingController.getSingleListing);
 
 module.exports = router;
