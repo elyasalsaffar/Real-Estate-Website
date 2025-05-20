9dee83267cb332086f40aaaefd4cab433fd6fb45
elyasalsaffar <elyas.dusty@gmail.com>
2025-05-20 20:56:32 +0300

    Worked on admin page with pending/ accepted/ rejected listings

--- controllers/listingController.js
+++ controllers/listingController.js
@@ -12,10 +12,30 @@ const getAllListings = async (req, res) => {
 
 const getAllRequests = async (req, res) => {
     try {
-        const listings = await Listing.find({ isApproved: {$in: [ null, undefined ]} });
-        res.render('./listings/requests.ejs', { listings });
+        const pendingListings = await Listing.find({ isApproved: {$in: [ null, undefined ]} });
+        const approvedListings = await Listing.find({ isApproved: true });
+        const rejectedListings = await Listing.find({ isApproved: false });
+        res.render('./listings/requests.ejs', { pendingListings, approvedListings, rejectedListings, user: req.user });
     } catch (error) {
         console.error('An error occurred getting all request listings!', error.message);
+    }
+};
+
+const approveListing = async (req, res) => {
+    try {
+        const updatedListing = await Listing.findByIdAndUpdate(req.params.id, { isApproved: true }, { new: true });
+        res.redirect('/listings/requests');
+    } catch (error) {
+        console.error('An error occurred accepting a pending listing!', error.message);
+    }
+};
+
+const rejectListing = async (req, res) => {
+    try {
+        const updatedListing = await Listing.findByIdAndUpdate(req.params.id, { isApproved: false }, { new: true });
+        res.redirect('/listings/requests');
+    } catch (error) {
+        console.error('An error occurred rejecting a pending listing!', error.message);
     }
 };
 
@@ -51,5 +71,7 @@ module.exports = {
     getAllListings,
     getSingleListing,
     getAllRequests,
-    getAllRequestListings
+    getAllRequestListings,
+    approveListing,
+    rejectListing
 }
--- routes/listingRouter.js
+++ routes/listingRouter.js
@@ -7,6 +7,10 @@ router.get('/all', listingController.getAllListings);
 
 router.get('/requests', listingController.getAllRequests);
 
+router.post('/requests/:id/approve', listingController.approveListing);
+
+router.post('/requests/:id/reject', listingController.rejectListing);
+
 router.get('/requestsList/:id', listingController.getAllRequestListings);
 
 router.get('/:id', listingController.getSingleListing);
